2a0892bad469d543ad8fbbbeb5032b19d9304bce
wangxingwen <wangxingwen@51testing.com>
2020-05-07 18:40:51 +0800

    细节修复

--- axios.js
+++ axios.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-axios.defaults.timeout = 5000;
-let pending = []; //声明一个数组用于存储每个ajax请求的取消函数和ajax标识
+axios.defaults.timeout = 30000;
+const pending = []; //声明一个数组用于存储每个ajax请求的取消函数和ajax标识
 let cancelToken = axios.CancelToken;
 let removePending = (ever) => {
-  if (pending.length<2) return
-  if (pending.length === 1) return
   for(let p = 0; p < pending.length; p++){
-      if(pending[p].u === ever.url + '&' + ever.method) { //当当前请求在数组中存在时执行函数体
+      if(pending[p].u === ever.url + '&' + ever.method + '&' + JSON.stringify(ever.params)) { //当当前请求在数组中存在时执行函数体
           pending[p].f(); //执行取消操作
           pending.splice(p, 1); //把这条记录从数组中移除
       }
@@ -17,11 +15,11 @@ let removePending = (ever) => {
 axios.interceptors.request.use(
   config => {
     // ------------------------------------------------------------------------------------
-    let str = JSON.stringify(config.params);
+    const str = JSON.stringify(config.params);
     removePending(config); //在一个ajax发送前执行一下取消操作
     config.cancelToken = new cancelToken((c)=>{
        // 这里的ajax标识我是用请求地址&请求方式拼接的字符串，当然你可以选择其他的一些方式
-       pending.push({ u: config.url + '&' + config.method + config.params, f: c });  
+       pending.push({ u: config.url + '&' + config.method+'&'+ str, f: c });  
     });
     // -----------------------------------------------------------------------------------------
     return config;
